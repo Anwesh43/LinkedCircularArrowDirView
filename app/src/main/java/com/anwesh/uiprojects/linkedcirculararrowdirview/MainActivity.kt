62add365448c94ec49796cf33de83534c9b76953
Anwesh43 <anweshthecool0@gmail.com>
2019-01-16 11:23:28 +0530

    using static create method in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedcirculararrowdirview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedcirculararrowdirview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedcirculararrowdirview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.circulararrowdirview.CircularArrowDirView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        CircularArrowDirView.create(this)
     }
 }
