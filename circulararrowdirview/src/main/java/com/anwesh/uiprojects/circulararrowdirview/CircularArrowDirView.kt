57ad6e685ee381f4e36df374b94b4522dc563b57
Anwesh43 <anweshthecool0@gmail.com>
2019-01-16 10:15:20 +0530

    adding Int and Float extension methods

--- circulararrowdirview/src/main/java/com/anwesh/uiprojects/circulararrowdirview/CircularArrowDirView.kt
+++ circulararrowdirview/src/main/java/com/anwesh/uiprojects/circulararrowdirview/CircularArrowDirView.kt
@@ -20,3 +20,9 @@ val scDiv : Double = 0.51
 val sizeFactor : Float = 2.8f
 val circleColor : Int = Color.parseColor("#f44336")
 val backColor : Int = Color.parseColor("#BDBDBD")
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
+fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
