2884dc0859fa8dffbdc847e988d6e33c25d61459
Anwesh43 <anweshthecool0@gmail.com>
2019-01-16 11:00:48 +0530

    adding CircularArrowDirView class

--- circulararrowdirview/src/main/java/com/anwesh/uiprojects/circulararrowdirview/CircularArrowDirView.kt
+++ circulararrowdirview/src/main/java/com/anwesh/uiprojects/circulararrowdirview/CircularArrowDirView.kt
@@ -74,3 +74,21 @@ fun Canvas.drawCADNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class CircularArrowDirView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
