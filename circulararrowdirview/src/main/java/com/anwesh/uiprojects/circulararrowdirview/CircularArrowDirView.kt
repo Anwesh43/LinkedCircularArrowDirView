b681a9528f2c3e51cd4c4557f87264d5fb866970
Anwesh43 <anweshthecool0@gmail.com>
2019-01-16 10:10:25 +0530

    importing libraries and adding constants

--- circulararrowdirview/src/main/java/com/anwesh/uiprojects/circulararrowdirview/CircularArrowDirView.kt
+++ circulararrowdirview/src/main/java/com/anwesh/uiprojects/circulararrowdirview/CircularArrowDirView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.circulararrowdirview
 /**
  * Created by anweshmishra on 16/01/19.
  */
+
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Path
+
+val nodes : Int = 5
+val arrows : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Float = 2.8f
+val circleColor : Int = Color.parseColor("#f44336")
+val backColor : Int = Color.parseColor("#BDBDBD")
